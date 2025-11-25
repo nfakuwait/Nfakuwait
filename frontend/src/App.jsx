3026d6d4fdc513d520dfb6aaf1f1b3287d96f4b2
Arun <720721110060@hicet.ac.in>
2025-11-25 12:07:46 +0530

    coming soon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,12 +13,13 @@ import InstallPrompt from "./components/InstallPrompt.jsx";
 import TranslateFloatingButton from "./components/TranslateFloatingButton.jsx";
 import Teacher from "./components/Teacher.jsx";
 import AdmissionForm from "./components/AdmissionForm.jsx";
+import Comingsoon from "./components/comingsoon.jsx";
 
 export default function App() {
   return (
     <>
       <Router>
-        <Header />
+        {/* <Header /> */}
         <Routes>
           <Route path="/login" element={<Login />}></Route>
           <Route
@@ -30,13 +31,16 @@ export default function App() {
               </ProtectedRoute>
             }
           ></Route>
-          <Route path="/about" element={<About />}></Route>
+            <Route path="/" element={<Comingsoon />}></Route>
+          {/* <Route path="/about" element={<About />}></Route>
           <Route path="/" element={<Homepage />}></Route>
           <Route path="/admission" element={<Admission />}></Route>
           <Route path="/gallery" element={<Gallery />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
           <Route path="/teacher" element={<Teacher />}></Route>
-          <Route path="/admissionform" element={<AdmissionForm />}></Route>
+        
+          
+          <Route path="/admissionform" element={<AdmissionForm />}></Route> */}
         </Routes>
       </Router>
       <InstallPrompt/>
